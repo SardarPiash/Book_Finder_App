2145bd65f1e56a30eb5a0eb86ff5f760d6082f1b
Sardar Piash <piashm66@gmail.com>
2024-04-08 13:49:44 +0600

    Book-Shelf Complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./header"
+import Footer from "./footer"
+import Herosection from "./herosection/herosection"
+import Bookshelf from "./bookshelf/bookshelf"
+
+export default function App() {
+  return (
+    <div>
+
+      <Header />
+      <Herosection />
+      <Bookshelf />
+      <Footer />
+    </div>
+  )
+}
---
+++ src/footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <div>
+      <footer class="py-6 md:py-8">
+        <div class="container mx-auto">
+          <p class="text-center text-sm lg:text-base">
+            Copyright ©2023 | All rights reserved by Sardar Piash
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
